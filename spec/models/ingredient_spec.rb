a7a96a1c454cdd96809364ebc17d5300011682de
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-29 09:21:58 -0600

    Created many many relationship between dishes and ingredients

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Ingredient, type: :model do
-  # describe "Relationships" do
-  #   it {should have_many :dish_ingredients}
-  #   it {should have_many :dishes, through: :dish_indredients}
+  describe "Relationships" do
+    it {should have_many :dish_ingredients}
+    it {should have_many(:dishes).through(:dish_ingredients)}
 
-  #end
+
+
+  end
 end
